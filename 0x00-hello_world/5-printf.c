ed08a69af6fa625edd9a08f5c41a5fe326d23e03
AtfiWiam <wiam.atfi@mail.com>
2023-07-13 09:42:26 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  *
  * Description : this function prints a message
  *
- * return: 0 (success)
+ * Return: 0 (success)
  *
  */
 int main(void)
 {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
