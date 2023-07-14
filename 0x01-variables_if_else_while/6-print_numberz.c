5d22feb24c165f863c05ca99782ea9d56d3997ce
AtfiWiam <wiam.atfi@mail.com>
2023-07-14 08:23:48 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints numbers of base 10
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
