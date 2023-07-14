7a43768c0522692e04103490000219e11c8a6143
AtfiWiam <wiam.atfi@mail.com>
2023-07-14 07:19:43 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabet except q and e
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c * (c!='q' && c!='e'));
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
