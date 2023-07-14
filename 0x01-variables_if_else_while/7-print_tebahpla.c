fb86879781be180cd8052020f2c53ba47848c76d
AtfiWiam <wiam.atfi@mail.com>
2023-07-14 08:34:15 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabet backwards
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
