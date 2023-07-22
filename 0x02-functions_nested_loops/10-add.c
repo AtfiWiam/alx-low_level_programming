cdef8cc7c422523803dc355be961895b1208bfb3
AtfiWiam <wiam.atfi@mail.com>
2023-07-22 08:31:38 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ 
+ * Return:0
+ */
+
+int add (int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+	int result = add(10, 20);
+
+	return (result);
+}
