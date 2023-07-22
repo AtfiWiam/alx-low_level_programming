c3fa61dcb0418fe6742897230538abef7b80809e
AtfiWiam <wiam.atfi@mail.com>
2023-07-22 09:24:28 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+			if (j > 0)
+			{
+				_putchar(' ');
+			}
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (result < 100)
+			{
+				_putchar(' ');
+			}
+			_putchar('0' + (result / 100));
+			_putchar('0' + ((result / 10) % 10));
+			_putchar('0' + (result % 10));
+		if (j < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+		}
+	}
+}
