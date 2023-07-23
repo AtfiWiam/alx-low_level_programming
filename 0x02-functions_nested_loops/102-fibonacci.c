338c12feed520c8676d646919112b4cba46d51cb
AtfiWiam <wiam.atfi@mail.com>
2023-07-23 08:50:25 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description : prints the first 50 Fibonacci numbers
+ * main - Entry point
+ * Description: prints the first 50 Fibonacci numbers
  * Return: 0
  */
 int main(void)
 {
-	int fib[50];
-	int n = 50;
-	int i = 2;
+	int i = 0;
+	long int a = 0, b = 1, fib;
 
-	fib[0] = 1;
-	fib[1] = 2;
-
-	while (i < n)
+	while (i < 50)
 	{
-		fib[i] = fib[i - 1] + fib[i - 2];
-		i++;
-	}
+		fib = a + b;
+		a = b;
+		b = fib;
+		printf("%lu", fib);
 
-	i = 0;
-	while (i < n)
-	{
-		printf("%d", fib[i]);
-		if (i < n - 1)
+		if (i < 49)
 		{
 			printf(", ");
 		}
 		i++;
 	}
-
 	printf("\n");
 	return (0);
 }
