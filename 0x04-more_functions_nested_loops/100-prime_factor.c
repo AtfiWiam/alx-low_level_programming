44427e1610567093c821a7e63b97fe6de630f5ad
AtfiWiam <wiam.atfi@mail.com>
2023-07-23 11:14:50 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point
+ * Description :  finds and prints the largest 
+ * prime factor of the number 612852475143
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long number = 612852475143;
+	long maxPrime = -1;
+	long i = 3;
+
+	while (number % 2 == 0)
+	{
+		maxPrime = 2;
+		number /= 2;
+	}
+
+	for (i = 3; i <= sqrt(number); i += 2)
+	{
+		 while (number % i == 0)
+		 {
+			 maxPrime = i;
+			 number /= i;
+		 }
+	}
+
+	if (number > 2)
+	{
+		maxPrime = number;
+	}
+	printf("%ld\n", maxPrime);
+	return (0);
+}
