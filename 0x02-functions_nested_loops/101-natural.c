7bcffc8905e1b4e38fc79bbd1e1f46482f4177d3
AtfiWiam <wiam.atfi@mail.com>
2023-07-23 08:33:54 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0;
+	int limit = 1024;
+	int i = 0;
+
+	while (i < limit)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
