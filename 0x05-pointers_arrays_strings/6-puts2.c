44a02102fb725a30f1d4492f9d9740624bb1a27d
AtfiWiam <wiam.atfi@mail.com>
2023-07-24 07:54:24 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,18 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (i >= 0)
 	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (i % 2 == 0)
+		{
 		_putchar(str[i]);
-		i += 2;
+		}
+		
+		i++;
 	}
-
-	_putchar('\n');
 }
