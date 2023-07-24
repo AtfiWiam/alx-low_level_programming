4b0f881097002f626503b6c8ecf542b2104ecde6
AtfiWiam <wiam.atfi@mail.com>
2023-07-24 08:06:29 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -32,12 +32,12 @@ void puts_half(char *str)
 
 	if (length % 2 == 0)
 	{
-		start_index = length / 2;
+		start_index = (length / 2) + 1;
 	}
 
 	else
 	{
-		start_index = (length - 1) / 2;
+		start_index = ((length - 1) / 2) + 1;
 	}
 
 	while (str[start_index] != '\0')
