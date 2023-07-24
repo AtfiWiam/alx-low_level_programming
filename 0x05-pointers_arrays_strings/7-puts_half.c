529abd139e43ce33e30cabb0d60b7f82a57d828e
AtfiWiam <wiam.atfi@mail.com>
2023-07-24 08:03:36 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen - function to calculate the length of a string
+ * @s: string value.
+ * Return: length.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+}
+
+/**
+ * puts_half - function that prints half of a string.
+ * @str: string value.
+ * Return: void.
+ */
+
+void puts_half(char *str)
+{
+	int length = _strlen(str);
+	int start_index;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+
+	while (str[start_index] != '\0')
+	{
+		_putchar(str[start_index]);
+		start_index++;
+	}
+
+	_putchar('\n');
+}
