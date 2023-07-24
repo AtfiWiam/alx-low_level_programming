104ca24a8e8cc38aaac7f4e8e97f251661e4ad0d
AtfiWiam <wiam.atfi@mail.com>
2023-07-24 09:26:57 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program,
+ * 101-crackme.
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int random_password = 0, a = 0;
+
+	time_t t;
+
+	srand((unsigned int) time(&t));
+
+	while (a < 2772)
+	{
+		random_password = rand() % 128;
+		if ((a + random_password) > 2772)
+		{
+			break;
+		}
+		a = a + random_password;
+		printf("%c", random_password);
+	}
+	printf("%c\n", (2772 - a));
+
+	return (0);
+}
