d16b65c8b61890e4644f1510559bdb0554d13cb3
AtfiWiam <wiam.atfi@mail.com>
2023-07-24 07:37:26 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,14 @@
 
 int _strlen(char *s)
 {
-	 int length = 0;
+	int length = 0;
 
-	 while (*s != '\0')
-	 {
-		 length++;
-		 s++;
-	 }
-	 return (length);
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
 
 /**
