55a59736e5936d44baa8207ff055cbee81684ae7
AtfiWiam <wiam.atfi@mail.com>
2023-07-27 08:14:22 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,41 +12,42 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int a = 0, b = 0, sum = 0;
+	int o, g, r1, r2;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
+	while (*(n1 + a) != '\0')
+		a++;
+	while (*(n2 + b) != '\0')
+		b++;
+	if (a >= b)
+		g = a;
 	else
-		bg = c2;
-	if (size_r <= bg + 1)
+		g = b;
+	if (size_r <= g + 1)
 		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	r[g + 1] = '\0';
+	a--, b--, size_r--;
+	r1 = *(n1 + a) - 48, r2 = *(n2 + b) - 48;
+	while (g >= 0)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
+		o = r1 + r2 + sum;
+		if (o >= 10)
+			sum = o / 10;
 		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
+			sum = 0;
+		if (o > 0)
+		*(r + g) = (o % 10) + 48;
 		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
+			*(r + g) = '0';
+		if (a > 0)
+			a--, r1 = *(n1 + a) - 48;
 		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
+			r1 = 0;
+		if (b > 0)
+			b--, r2 = *(n2 + b) - 48;
 		else
-			dr2 = 0;
-		bg--, size_r--;
+			r2 = 0;
+		g--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
