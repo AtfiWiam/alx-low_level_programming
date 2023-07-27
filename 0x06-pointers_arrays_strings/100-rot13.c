61a6c4db6c3cbb2bce0477796ee0c048c2fa91cd
AtfiWiam <wiam.atfi@mail.com>
2023-07-27 07:10:07 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @str: string.
+ *
+ * Return: pointer to str.
+ */
+
+char *rot13(char *str)
+{
+	char *ptr = str;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
+		{
+			str[i] += 13;
+		}
+		else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
+		{
+			str[i] -= 13;
+		}
+	}
+	return (ptr);
+}
