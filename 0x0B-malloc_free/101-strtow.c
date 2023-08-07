287ba6f09dfe8b72164e5a2995c39d75fb873ac5
AtfiWiam <wiam.atfi@mail.com>
2023-08-07 14:09:08 -0700

    0x0B. C - malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,48 +39,51 @@ int count_words(char *str)
  */
 char **strtow(char *str)
 {
-	int wc = count_words(str), wi = 0, wl = 0, iw = 0, i, j;
-	char **w = (char **)malloc((wc + 1) * sizeof(char *));
+	int word_count = count_words(str), word_index = 0, word_length = 0, is_word = 0, i, j;
+	char **words = (char **)malloc((word_count + 1) * sizeof(char *));
 
-	if (!str || !*str) 
+	if (str == NULL || *str == '\0' || word_count == 0 || words == NULL)
 		return (NULL);
-	if (!w) 
-		return (NULL);
-	for (i = 0; str[i]; ++i)
+	for (i = 0; str[i] != '\0'; ++i)
 	{
 		if (str[i] != ' ')
 		{
-			if (!iw) 
-				iw = wl = 1;
-			else wl++;
+			if (!is_word)
+				is_word = 1, word_length = 0;
+			word_length++;
 		}
-		else if (iw)
+		else if (is_word)
 		{
-			w[wi] = (char *)malloc((wl + 1) * sizeof(char));
-			if (!w[wi])
+			words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+			if (words[word_index] == NULL)
 			{
-				for (j = 0; j < wi; ++j) 
-					free(w[j]);
-				free(w);
+				for (j = 0; j < word_index; ++j)
+				{
+					free(words[j]);
+				}
+				free(words);
 				return (NULL);
 			}
-			strncpy(w[wi], str + i - wl, wl);
-			w[wi++][wl] = iw = 0;
+		strncpy(words[word_index], str + i - word_length, word_length);
+		words[word_index][word_length] = '\0';
+		word_index++, is_word = 0;
 		}
 	}
-	if (iw)
+	if (is_word)
 	{
-		w[wi] = (char *)malloc((wl + 1) * sizeof(char));
-		if (!w[wi])
+		words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+		if (words[word_index] == NULL)
 		{
-			for (j = 0; j < wi; ++j) 
-				free(w[j]);
-			free(w);
+			for (j = 0; j < word_index; ++j)
+				free(words[j]);
+			free(words);
 			return (NULL);
 		}
-		strncpy(w[wi], str + strlen(str) - wl, wl);
-		w[wi][wl] = 0;
+		strncpy(words[word_index], str + strlen(str) - word_length, word_length);
+		words[word_index][word_length] = '\0';
+		word_index++;
 	}
-	w[wi] = NULL;
-	return (w);
+	words[word_index] = NULL;
+	return (words);
+
 }
