5bfd34306547526b9e7bc10ec0545124888e39c6
AtfiWiam <wiam.atfi@mail.com>
2023-08-11 05:55:58 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,28 +15,24 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-	unsigned int min_size;
-
-	if (ptr == NULL)
-		return (malloc(new_size));
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+	}
+
 	if (new_size == old_size)
-		return (NULL);
+	{
+		return (ptr);
+	}
 
-	new_ptr = malloc(new_size);
-
-	if (new_ptr == NULL)
-		return (NULL);
-
-	min_size = (old_size < new_size) ? old_size : new_size;
-
-	memcpy(new_ptr, ptr, min_size);
 	free(ptr);
+	ptr = malloc(new_size);
 
-	return (new_ptr);
+	return (ptr);
 }
