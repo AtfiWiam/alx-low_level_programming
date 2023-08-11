3859ebea707aedadad4d3d1acc02012add643f09
AtfiWiam <wiam.atfi@mail.com>
2023-08-11 06:38:25 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,30 +4,30 @@
 #include <ctype.h>
 
 /**
- * _iszero - checks if any number is zero
+ * _is_zero - function that check if any number is zero
  * @argv: command line arguments.
  *
- * Return: nothing.
+ * Return: no return.
  */
-void _iszero(char *argv[])
+void _is_zero(char *argv[])
 {
-	int i, n1 = 1, n2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] != '0')
 		{
-			n1 = 0;
+			isn1 = 0;
 			break;
 		}
 
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
-			n2 = 0;
+			isn2 = 0;
 			break;
 		}
 
-	if (n1 == 1 || n2 == 1)
+	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
 		exit(0);
@@ -35,40 +35,41 @@ void _iszero(char *argv[])
 }
 
 /**
- * _inarray - initialize a new array to zero.
- * @arr: array of char.
- * @len: length of the array
+ * _init_array - function that set memery to zero in a new array
+ * @ar: array of char.
+ * @lar: length of the array of char.
  *
- * Return: pointer to the array.
+ * Return: pointer to the array of char.
  */
-char *_inarray(char *arr, int len)
+char *_init_array(char *ar, int lar)
 {
 	int i = 0;
 
-	for (i = 0; i < len; i++)
-		arr[i] = '0';
-	arr[len] = '\0';
-	return (arr);
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
 }
 
 /**
- * _check - check the length of the number and if it is in base 10.
+ * _checknumber - function that check the length of the number
+ * and checks if number is in base 10.
  * @argv: command line arguments.
- * @r: row of the array.
+ * @n: row of the array.
  *
  * Return: length of the number.
  */
-int _check(char *argv[], int r)
+int _checknumber(char *argv[], int n)
 {
-	int len;
+	int ln;
 
-	for (len = 0; argv[r][len]; len++)
-		if (!isdigit(argv[r][len]))
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
 			printf("Error\n");
 			exit(98);
 		}
-	return (len);
+	return (ln);
 }
 
 /**
@@ -81,45 +82,44 @@ int _check(char *argv[], int r)
  */
 int main(int argc, char *argv[])
 {
-	int len1, len2, lenout, sum, suml, i, j, k, ca;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	len1 = _check(argv, 1), len2 = _check(argv, 2);
-	_iszero(argv), lenout = len1 + len2, nout = malloc(lenout + 1);
+	ln1 = _checknumber(argv, 1), ln2 = _checknumber(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	nout = _inarray(nout, lenout);
-	k = lenout - 1, i = len1 - 1, j = len2 - 1, ca = suml = 0;
+	nout = _init_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
 		{
-			if (suml > 0)
+			if (addl > 0)
 			{
-				sum = (nout[k] - '0') + suml;
-				if (sum > 9)
-					nout[k - 1] = (sum / 10) + '0';
-				nout[k] = (sum % 10) + '0';
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
-			i = len1 - 1, j--, suml = 0, ca++, k = lenout - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
 			if (nout[0] != '0')
 				break;
-			lenout--;
-			free(nout), nout = malloc(lenout + 1), nout = _inarray(nout, lenout);
-			k = lenout - 1, i = len1 - 1, j = len2 - 1, ca = suml = 0;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _init_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
-			sum = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + suml;
-			suml = sum / 10, nout[k] = (sum % 10) + '0';
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
 	printf("%s\n", nout);
 	return (0);
 }
-
