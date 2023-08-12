1449b96bae607b79e0cb6f1b912cf7aaac0c0d9a
AtfiWiam <wiam.atfi@mail.com>
2023-08-12 06:55:30 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,73 +2,76 @@
 #include <stdlib.h>
 
 /**
- * _atoi_digit - convert a char to integer.
- * @x: character to convert.
+ * _todigit - convert a char to integer.
+ * @c: char to convert.
  * Return: integer.
  **/
 
-int _atoi_digit(char x)
+int _todigit(char c)
 {
-	unsigned int res;
+	unsigned int i;
 
-	if (x <= '9' && x >= '0')
-		res = x - '0';
-	return (res);
+	if (c <= '9' && c >= '0')
+		i = c - '0';
+	return (i);
 }
 
 /**
- * _isNumber - Define if a string is a number.
+ * _isNumber - check if a string is a number.
  * @argv: Pointer to string.
  * Return: success (0).
  **/
+
 int _isNumber(char *argv)
 {
-	int i;
+	int n;
 
-	for (i = 0; argv[i]; i++)
-		if (argv[i] < 48 || argv[i] > 57)
+	for (n = 0; argv[n]; n++)
+	{
+		if (argv[n] < 48 || argv[n] > 57)
 			return (1);
+	}
 	return (0);
 }
 
 /**
- *_calloc - allocate array of size * nmemb.
- * @nmemb: number of elements.
+ *_calloc - allocate array of size x.
+ * @x: number of elements.
  * @size: size of element.
  * Return: pointer to array.
  **/
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+void *_calloc(unsigned int x, unsigned int size)
 {
-	char *tab;
+	char *arr;
 	unsigned int i;
 
-	tab = malloc(size * nmemb);
+	arr = malloc(size * x);
 
-	if (tab == NULL)
+	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < (size * nmemb); i++)
-		tab[i] = '0';
+	for (i = 0; i < (size * x); i++)
+		arr[i] = '0';
 
-	return (tab);
+	return (arr);
 }
 
 /**
- * mul_array - multiply two arrays.
+ * _multiply - multiply two arrays.
  * @a1: first array.
  * @len1: length of array a1.
- * @a2:  char.
+ * @a2: second array
  * @a3: array for result.
- * @lena: length of array a3.
- * Return: pointer to array.
+ * @len3: length of array a3.
+ * Return: pointer to result array.
  **/
 
-void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
+void *_multiply(char *a1, int len1, char a2, char *a3, int len3)
 {
 	int mul = 0, i, k;
 
-	k = lena;
+	k = len3;
 	for (i = len1 - 1; i >= 0 ; i--)
 	{
 		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
@@ -87,22 +90,24 @@ void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
 
 	return (a3);
 }
+
 /**
- * print_array - print all digits of array.
- * @nb: number of elements to print.
- * @a: array of elements.
+ * print_arr - prints digits of array.
+ * @n: number of elements to print.
+ * @arr: array of elements.
  **/
-void print_array(char *a, int nb)
+
+void print_arr(char *arr, int n)
 {
 	int i = 0;
 
-	while (a[i] == '0' && (i + 1) < nb)
+	while (arr[i] == '0' && (i + 1) < n)
 	{
 		i++;
 	}
-	for (; i < nb; i++)
+	for (; i < n; i++)
 	{
-		_putchar(a[i]);
+		_putchar(arr[i]);
 	}
 	_putchar('\n');
 }
@@ -116,9 +121,9 @@ void print_array(char *a, int nb)
 
 int main(int argc, char *argv[])
 {
-	int i, c, len1, len2, lenres;
+	int i, c, len1, len2, lres;
 	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
-	char *tabres;
+	char *ares;
 
 	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
 	{
@@ -128,24 +133,24 @@ int main(int argc, char *argv[])
 		}
 		exit(98);
 	}
-	for (len1 = 0; argv[1][len1]; len1++)
-	;
-	for (len2 = 0; argv[2][len2]; len2++)
-	;
-	lenres = len1 + len2;
-	tabres = _calloc(lenres, sizeof(int));
-	if (tabres == NULL)
+	for (len1 = 0; argv[1][len1]; len1++);
+	for (len2 = 0; argv[2][len2]; len2++);
+
+	lres = len1 + len2;
+	ares = _calloc(lres, sizeof(int));
+
+	if (ares == NULL)
 	{
-		free(tabres);
+		free(ares);
 		return (0);
 	}
 	for (i = len2 - 1, c = 0; i >= 0; i--)
 	{
-	tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
+	ares = _multiply(argv[1], len1, argv[2][i], ares, (lres - 1 - c));
 	c++;
 	}
-	print_array(tabres, lenres);
-	free(tabres);
+	print_arr(ares, lres);
+	free(ares);
 	exit(EXIT_SUCCESS);
 	return (0);
 }
