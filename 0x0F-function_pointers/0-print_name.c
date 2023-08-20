1a35a35cf23d73de3d43277165c66c84371e9763
AtfiWiam <wiam.atfi@mail.com>
2023-08-20 07:59:50 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name
+ * @name: name
+ * @f: function
+ * Return : nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		while (*name != '\0')
+		{
+			f(name);
+			name++;
+		}
+	}
+}
