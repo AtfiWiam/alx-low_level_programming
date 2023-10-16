b195f40014e7d5e28e7e64ad0888a6c733020d9e
AtfiWiam <wiam.atfi@mail.com>
2023-10-16 13:38:45 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/AtfiWiam/alx-low_level_programming/blob/e58dc35ecea2db15dee6d84332d58be1f9d0c324/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+gcc -shared -o win.so -fPIC win.c
+export LD_PRELOAD=./win.so:$LD_LIBRARY_PATH
